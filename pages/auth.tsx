b79fdc475a5d3c4713207ad981c9d6db378b0a65
kobra-soft <code.softwaredev@gmail.com>
2024-04-16 17:13:24 +0100

    Auth - Github setup

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -4,6 +4,9 @@ import { useCallback, useState } from "react";
 import { useRouter } from "next/router";
 import { signIn } from "next-auth/react";
 
+import { FcGoogle } from 'react-icons/fc';
+import { FaGithub } from 'react-icons/fa';
+
 const Auth = () => {
   const router = useRouter();
 
@@ -84,6 +87,15 @@ const Auth = () => {
             <button onClick={variant === 'login' ? login : register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
             {variant === 'login' ? 'Sign in' : 'Register'}
             </button>
+
+            <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+              <div onClick={() => signIn('google', { callbackUrl: '/profiles' })} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                <FcGoogle size={32} />
+              </div>
+              <div onClick={() => signIn('github', { callbackUrl: '/profiles' })} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                <FaGithub size={32} />
+              </div>
+              </div>
             <p className="text-neutral-500 mt-12">
               {variant === 'login' ? 'First time using Netflix?' : 'Already have an account?'}
               <span onClick={toggleVariant} className="text-white ml-2 hover:underline cursor-pointer">
