c71b32cc5ab9ed36d15ff999680b4b71f958acea
kobra-soft <code.softwaredev@gmail.com>
2024-04-22 13:00:33 +0100

    Commit

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -8,16 +8,62 @@ import { FaGithub } from "react-icons/fa";
 interface InputProps {
   // ...other props
   onKeyDown?: (event: React.KeyboardEvent) => void;
+  label: string;
 }
 
 const InputWithKeyDown: React.FC<
   InputProps & React.InputHTMLAttributes<HTMLInputElement>
-> = ({ onKeyDown, ...otherProps }) => {
+> = ({ onKeyDown, id, label, type, ...otherProps }) => {
   return (
-    <input
-      // ...otherProps
-      onKeyDown={onKeyDown}
-    />
+    <>
+      <div className="relative">
+        <input
+          id={id}
+          type={type}
+          {...otherProps}
+          onKeyDown={onKeyDown}
+          className="
+          block
+          rounded-md
+          px-6
+          pt-6
+          pb-1
+          w-full
+          text-md
+          text-white
+          bg-neutral-700
+          appearance-none
+          focus:outline-none
+          focus:ring-0
+          peer
+          invalid:border-b-1
+        "
+          placeholder=" "
+        />
+        <label
+          className="
+          absolute 
+          text-md
+          text-zinc-400
+          duration-150 
+          transform 
+          -translate-y-3 
+          scale-75 
+          top-4 
+          z-10 
+          origin-[0] 
+          left-6
+          peer-placeholder-shown:scale-100 
+          peer-placeholder-shown:translate-y-0 
+          peer-focus:scale-75
+          peer-focus:-translate-y-3
+        "
+          htmlFor={id}
+        >
+          {label}
+        </label>
+      </div>
+    </>
   );
 };
 
@@ -74,12 +120,12 @@ const Auth = () => {
             </h2>
             <div className="flex flex-col gap-4">
               {variant === "register" && (
-              <Input
-                id="name"
-                label="Username"
-                value={name}
-                onChange={(ev: any) => setName(ev.target.value)}
-              />
+                <Input
+                  id="name"
+                  label="Username"
+                  value={name}
+                  onChange={(ev: any) => setName(ev.target.value)}
+                />
               )}
               <Input
                 id="email"
@@ -88,17 +134,24 @@ const Auth = () => {
                 value={email}
                 onChange={(ev: any) => setEmail(ev.target.value)}
               />
-              <Input
+{/*               <Input
                 id="password"
                 label="Password"
                 type="password"
                 value={password}
                 onChange={(ev: any) => setPassword(ev.target.value)}
-                /* onKeyDown={(ev: any) => {
+              /> */}
+              <InputWithKeyDown
+                id="password"
+                label="Password"
+                type="password"
+                value={password}
+                onChange={(ev: any) => setPassword(ev.target.value)}
+                onKeyDown={(ev: any) => {
                   if (ev.key === "Enter") {
                     variant === "login" ? login() : register();
                   }
-                }} */
+                }}
               />
             </div>
             <button
